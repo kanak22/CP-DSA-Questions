7a50cc53a76395aa1704428ea909be98fb18290e
gunjan-g <gunjang1202@gmail.com>
2021-10-01 02:47:37 +0530

    added test cases and comments

--- HackerRank/Designer PDF Viewer/DesignerPDFViewer.cpp
+++ HackerRank/Designer PDF Viewer/DesignerPDFViewer.cpp
@@ -1,3 +1,25 @@
+/*
+Problem Link:
+https://www.hackerrank.com/challenges/designer-pdf-viewer/problem
+
+Ques.
+When a contiguous block of text is selected in a PDF viewer, the selection is highlighted with a blue rectangle. In this PDF viewer, each word is highlighted independently.
+There is a list of 26 character heights aligned by index to their letters. For example, 'a' is at index 0 and 'z' is at index 25. There will also be a string. Using the letter heights given, determine the area of the rectangle highlight in mm^2 assuming all letters are 1mm wide.
+
+Sample Input:
+1 3 1 3 1 4 1 3 2 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5
+abc
+
+Sample Output 0:
+9
+
+Explanation 0:
+We are highlighting the word abc:
+
+Letter heights are a=1, b=3 and c=1. The tallest letter, b, is 3mm high. The selection area for this word is 3.1.3mm= 9mm^3.
+Note: Recall that the width of each character is 1mm.
+*/
+
 #include <bits/stdc++.h>
 #include<string.h>
 using namespace std;
@@ -19,14 +41,19 @@ int designerPdfViewer(vector<int> h, string word) {
     int size=word.size(),i;
     vector<int> data;
     data.resize(size);
-    for(i=0;i<size;i++){
+
+    for(i=0;i<size;i++){      //filling the contents of vector data to find the index of alphabets
       data[i] = word[i]-'a';
     } 
     
-    int max= h[data[0]];
+    int max= h[data[0]];    //setting max to the height of first element present in data
+
+    //finding max height from all the alphabets present in string word
     for(i=1;i<size;i++){
         if(h[data[i]]>max) max=h[data[i]];
     }
+
+    //area is given as max height of word * the breadth (which is given by size of word)
     int area = max*size;
     return area;
 }
