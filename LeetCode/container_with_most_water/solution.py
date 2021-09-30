51b7a936e9b0e03a8072ef4719252132f0e6dcb8
KruzeZab <kruzezab4@gmail.com>
2021-10-01 01:55:07 +0545

    Container With Most Water added comment - Leetcode (python)

--- LeetCode/container_with_most_water/solution.py
+++ LeetCode/container_with_most_water/solution.py
@@ -14,11 +14,14 @@ class Solution:
         
         while i < j:
             if height[i] < height[j]:
+                # if maxuimum is smaller than current container
+                # Assign maximum to the the higher value 
                 maximum = max(maximum, (j-i) * height[i])
-                i += 1
+                i += 1 # Increase index
             else:
+                # if maxuimum is smaller than current container
+                # Assign maximum to the the higher value 
                 maximum = max(maximum, (j-i) * height[j])
-                j -= 1
+                j -= 1 # Decrease index
 
         return maximum
-        
