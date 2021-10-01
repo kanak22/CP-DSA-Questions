f18281ad0d5b028701f7b49f9f8db9d02e40496e
Debjit Pal <debjitpal5040@gmail.com>
2021-10-01 16:27:58 +0530

    Update closest-number.py

--- HackerRank/Closest Number/closest-number.py
+++ HackerRank/Closest Number/closest-number.py
@@ -1,3 +1,8 @@
+"""First calculate the power and the remainder. As from the constraints the index can be negative too.
+So we can't apply the inbuilt pow() function for computing the reaminder. Just using the basic operators is sufficient.
+Then if the remainder is greater than half of x, compute the value (remainder - x) and subtract that from power.
+Else just subtract the remainder from the power. That value is the expected result
+"""
 T = int(input())
 for _ in range(T):
     a, b, x = map(int, input().split())
