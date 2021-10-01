f6412ef657f68c4929e8fc91050023014d66068e
Atish Kumar Prasad <atishprasad90@gmail.com>
2021-10-01 21:43:43 +0530

    Update

--- LeetCode/Integer_To_Roman/solution.cpp
+++ LeetCode/Integer_To_Roman/solution.cpp
@@ -2,9 +2,11 @@
 
 using namespace std;
 
+// Function to convert the number to roman numeral
 string intToRoman(int number) {
 
     const int num[] = {1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000};
+    // Possible Roman Alphabets
     const string sym[] = {"I", "IV", "V", "IX", "X", "XL", "L", "XC", "C", "CD", "D", "CM", "M"};
     int i = 13;
     string ans = "";
@@ -21,6 +23,7 @@ string intToRoman(int number) {
     return ans;
 }
 
+// Driver Code
 int main() {
 
     int N;
