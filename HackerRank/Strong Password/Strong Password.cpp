5b23f9e82dbb835804fa571d63adfffedffb6048
gunjan-g <gunjang1202@gmail.com>
2021-10-01 21:07:41 +0530

    added code

---
+++ HackerRank/Strong Password/Strong Password.cpp
@@ -0,0 +1,74 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+/*
+ * Complete the 'minimumNumber' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER n
+ *  2. STRING password
+ */
+
+int minimumNumber(int n, string password) {
+    int i,arr[4]={0},count=0;
+    for(i=0;password[i]!='\0';i++){
+        if(isdigit(password[i])) arr[0]++;
+        else if(islower(password[i])) arr[1]++;
+        else if(isupper(password[i])) arr[2]++;
+        else arr[3]++;
+    }
+    for(i=0;i<4;i++){
+        if(arr[i]==0) count++;
+    }
+    int x= 6-(n+count);
+    if(x>0) count+=x;
+return count;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string n_temp;
+    getline(cin, n_temp);
+
+    int n = stoi(ltrim(rtrim(n_temp)));
+
+    string password;
+    getline(cin, password);
+
+    int answer = minimumNumber(n, password);
+
+    fout << answer << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
