9fd6e97f0f6bc26627a45a29cd9159bc57b71868
gunjan-g <gunjang1202@gmail.com>
2021-10-01 22:58:33 +0530

    added test cases

--- HackerRank/Find Median/Find Median.cpp
+++ HackerRank/Find Median/Find Median.cpp
@@ -1,3 +1,27 @@
+/*
+Programming Link:
+https://www.hackerrank.com/challenges/find-the-median/problem
+
+Ques.
+The median of a list of numbers is essentially its middle element after 
+rting. The same number of elements occur after it as before. Given a list 
+of numbers with an odd number of elements, find the median?
+
+Example.
+arr= [5,3,1,2,4]
+The sorted array arr' =[1,2,3,4,5]. The middle element and the median is 3.
+
+Sample Input 0:
+7
+0 1 2 4 6 5 3
+
+Sample Output 0:
+3
+
+Explanation 0:
+The sorted arr'=[0,1,2,3,4,5,6]. It's middle element is at 3.
+*/
+
 #include <bits/stdc++.h>
 
 using namespace std;
