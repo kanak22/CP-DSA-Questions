216cb3fceccaa5c0b3b73e0bb5b87aa079cc7ae6
Seun Taiwo <45892107+dro-1@users.noreply.github.com>
2021-10-02 09:10:08 +0100

    Added Comments

--- LeetCode/Container With Most Water/Container With Most Water.js
+++ LeetCode/Container With Most Water/Container With Most Water.js
@@ -1,18 +1,32 @@
+//Time Complexity - O(n)
+//Space Complexity - O(1)
+
 const containerWithMostWater = (height) => {
+
+  //Initialize pointers and maxArea Variable
   let l = 0;
   let r = height.length - 1;
   let maxArea = 0;
+
+  //Loop till pointers equal each other
   while (l < r) {
+    
+    //Calculate the area for each 2 bars
     let area = Math.min(height[l], height[r]) * (r - l);
+    
+    //Set maxArea if current area is greater than itself
     if (area > maxArea) {
       maxArea = area;
     }
+    
+    //Move pointer pointing to lower value as only that can increase our area
     if (height[l] < height[r]) {
       l++;
     } else {
       r--;
     }
   }
+
   return maxArea;
 };
 
