da74a38cd71c51f137455c3b8a91ee9a527bdbe2
manognyaa <77045147+manognyaa@users.noreply.github.com>
2021-10-02 18:12:03 +0530

    Graph_Coloring.cpp

---
+++ 450 DSA/Graph Coloring/Graph_Coloring.cpp
@@ -0,0 +1,90 @@
+#======================================================================================================================================================================
+# Python3 program to implement greedy
+# algorithm for graph coloring
+#======================================================================================================================================================================
+
+def addEdge(adj, v, w):
+	
+	adj[v].append(w)
+	
+	# Note: the graph is undirected
+	adj[w].append(v)
+	return adj
+  
+#======================================================================================================================================================================
+# now we will Assign the colors starting from 0 to all and also vertices will prints the assignment of colors
+#======================================================================================================================================================================
+def greedyColoring(adj, V):
+	
+	result = [-1] * V
+#======================================================================================================================================================================
+	# now, we are Assigning the first color to first vertex
+#======================================================================================================================================================================
+	
+  result[0] = 0;
+
+#======================================================================================================================================================================
+	# we need a temporary array to store the available colors. True value of available[cr] would mean that the color cr is assigned to one of its adjacent vertices
+#======================================================================================================================================================================
+
+	available = [False] * V
+
+	# Assign colors to remaining V-1 vertices
+	for u in range(1, V):
+		
+		# Process all adjacent vertices and
+		# flag their colors as unavailable
+		for i in adj[u]:
+			if (result[i] != -1):
+				available[result[i]] = True
+
+		#let us find now the first available color
+		cr = 0
+		while cr < V:
+			if (available[cr] == False):
+				break
+			
+			cr += 1
+			
+		# we can now assign the found color
+		result[u] = cr
+
+		# Reset the values back to false for the next iteration
+		for i in adj[u]:
+			if (result[i] != -1):
+				available[result[i]] = False
+
+	# Pint the result
+	for u in range(V):
+		print("Vertex", u, " ---> Color", result[u])
+
+#======================================================================================================================================================================
+# from now, we will have the Driver Code
+#======================================================================================================================================================================
+      
+if __name__ == '__main__':
+	
+	g1 = [[] for i in range(5)]
+	g1 = addEdge(g1, 0, 1)
+	g1 = addEdge(g1, 0, 2)
+	g1 = addEdge(g1, 1, 2)
+	g1 = addEdge(g1, 1, 3)
+	g1 = addEdge(g1, 2, 3)
+	g1 = addEdge(g1, 3, 4)
+	print("Coloring of graph 1 ")
+	greedyColoring(g1, 5)
+
+	g2 = [[] for i in range(5)]
+	g2 = addEdge(g2, 0, 1)
+	g2 = addEdge(g2, 0, 2)
+	g2 = addEdge(g2, 1, 2)
+	g2 = addEdge(g2, 1, 4)
+	g2 = addEdge(g2, 2, 4)
+	g2 = addEdge(g2, 4, 3)
+	print("\nColoring of graph 2")
+	greedyColoring(g2, 5)
+    
+#======================================================================================================================================================================    
+   
+    
+
