9c58100dc783651edca8ee531f9d3380ff05395e
Jisha Naik <84335748+jishanaik@users.noreply.github.com>
2021-10-05 01:07:38 +0530

    Update and rename intrep.cpp to Interesting Representation.cpp

--- Codechef/Interesting Representation/Interesting Representation.cpp
+++ Codechef/Interesting Representation/Interesting Representation.cpp
@@ -4,26 +4,31 @@
 using namespace std;
 #define ll long long
 
-int v[] = {3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73};
+/*Array of all prime numbers from 1 to 100*/
+int v[] = {3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97};
 
 int main() {
 	
 	int t;
-	cin >> t;
+	cin >> t; /*Input number of test cases*/
 	for (int i = 0; i < t; i++) {
 		ll n;
-		cin >> n;
+		cin >> n; /*Input n*/
+		
+		/*For even numbers*/
 		if (n % 2 == 0) {
 			ll a = 2 * n;
 			ll b = n;
-			cout << a << " " << b;
-		} else {
+			cout << a << " " << b; /*Print the two numbers*/
+		}
+		/*For odd numbers*/
+		else {
 			int j = 0;
 			while (n % v[j] == 0)
 				j++;
 			ll a = n * v[j];
 			ll b = n * (v[j] - 1);
-			cout << a << " " << b;
+			cout << a << " " << b; /*Print the two numbers*/
 		}
 		cout << "\n";
 	}
