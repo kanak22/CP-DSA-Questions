c273a5ce0b00c17dabfe3600449155c7f6e9c705
Aditya Aggarwal <85849078+adityaaggarwal12@users.noreply.github.com>
2021-10-05 01:56:29 +0530

    Update 276 B Little Girl and Game.cpp

--- Codeforces/Rating 1300/276 B Little Girl and Game.cpp
+++ Codeforces/Rating 1300/276 B Little Girl and Game.cpp
@@ -7,9 +7,11 @@ int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);
     string h;
+    // string h is the main input string
     set<char> s;
     int x,odd=0,even=0;
     cin >> h;
+    //here after this loop set 's' will have every unique element of string 'h'
     for(int i=0;i<h.size();i++){
         s.insert(h[i]);
     }
@@ -20,6 +22,8 @@ int main(){
         if(x%2==0){even++;}
         else{odd++;}
     }
+    // in above loop i have counted how many times every unique element of set 's' repeats in string 'h'
+    //as a palindrome needs only one or zero odd ements , so if there are 1 or 0 odd elements first will win and if odd elenents are more than 1 then it will be decided by whose turn it is
     if(odd<2){
         cout << "First";
         return 0;
