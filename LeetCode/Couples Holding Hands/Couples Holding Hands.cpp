b8b3a7a0b6c28c5d9ef1efe66a24dbb674b64992
Amit Dutta <amitdutta2406@gmail.com>
2021-10-07 00:59:25 +0530

    Added Couples Holding Hands in CPP

--- LeetCode/Couples Holding Hands/Couples Holding Hands.cpp
+++ LeetCode/Couples Holding Hands/Couples Holding Hands.cpp
@@ -1,9 +1,9 @@
-/*
+/**
 Think about each couple as a vertex in the graph. So if there are N couples, there are N vertices. Now if in position 2i and 2i +1 there are person from couple u and couple v sitting there, that means that the permutations are going to involve u and v. So we add an edge to connect u and v. The min number of swaps = N - number of connected components.
-*/
+**/
 class Solution {
 public:
-    int par[65];
+    int par[65]; // parent array
     int minSwapsCouples(vector<int>& row) {
         memset(par, -1, sizeof(par));
         int n = row.size()/2;
