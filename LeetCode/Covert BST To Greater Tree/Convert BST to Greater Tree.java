91a6cefad21837b556a2a8733e090650647b8c37
yash <65124321+yashg41@users.noreply.github.com>
2021-10-10 11:56:26 +0530

    Update Convert BST to Greater Tree.java
    Added comments

--- LeetCode/Covert BST To Greater Tree/Convert BST to Greater Tree.java
+++ LeetCode/Covert BST To Greater Tree/Convert BST to Greater Tree.java
@@ -1,22 +1,39 @@
 class Solution {
+    //global variable to store sum
     static int sum=0;
+    
     public void convertToGreater(TreeNode root){
+        
+        //if root is null then return 
        if(root==null)return;
+        
+        //if it is a leaf node
        if(root.left==null && root.right==null){
+           //temporary variable to store root value
            int prev=root.val;
+           //adding sum value to root
            root.val+=sum;
+           //adding prev value to sum
            sum+=prev;
            return; 
        };
+       //calling right subtree
        convertToGreater(root.right);
+        //temporary variable to store root value
        int prev=root.val;
+        //adding sum value to root
        root.val+=sum;
+        //adding prev value to sum
        sum+=prev;
+        //calling left sub tree
        convertToGreater(root.left);
     }
     public TreeNode convertBST(TreeNode root) {
+        //calling helper function
         convertToGreater(root);
+        
         sum=0;//restting to default value since this is a static variable
+        
         return root;
     }
 }
