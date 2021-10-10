b1299e840d8fcbd90e1821bb81bca80997b7340d
Somanshu693 <wilsonsam693@gmail.com>
2021-10-10 20:10:27 +0530

    Added solution in java(comments and updated files)

--- LeetCode/Kth Smallest Element in a Sorted Matrix/KthSmallestElementInASortedMatrix.java
+++ LeetCode/Kth Smallest Element in a Sorted Matrix/KthSmallestElementInASortedMatrix.java
@@ -1,7 +1,17 @@
 class Solution {
     public int kthSmallest(int[][] matrix, int k) {
-        int m = matrix.length, n = matrix[0].length;
-        int low = matrix[0][0], high = matrix[m-1][n-1];
+        int m = matrix.length, n = matrix[0].length;  // m = rows n = columns
+        int low = matrix[0][0], high = matrix[m-1][n-1]; 
+        //Using Binary Search
+        //The mid is just a value calculated by using low and high, 
+        //not a true element in the matrix, so when count < k, it means that the mid 
+        //is small, let low = mid + 1 to find the true element in matrix one by one. 
+        //And when count > k, it means that the mid is big enough, let high = mid to 
+        //ensure the element we need is between low and high(mid). And when count = k, 
+        //it means that there are k elements less than or equal to mid in the matrix, 
+        //but we don't know whether the element we need is equal to mid. 
+        //So let high = mid, it can ensure that the element we need is between low and high(mid).
+        
         while(low < high){
             int mid = (high - low)/2 + low;
             int count = 0;
--- LeetCode/Plus One/PlusOne.java
+++ LeetCode/Plus One/PlusOne.java
@@ -1,17 +1,18 @@
 class Solution {
     public int[] plusOne(int[] digits) {
         int carry = 1;
+        // start the loop from last index
         for (int i = digits.length - 1; i >= 0 && carry == 1; i--) {
             int temp = digits[i] + carry;
-            if (temp >= 10) {
-                carry = 1;
+            if (temp >= 10) {  // if number is greater than 10 we will add 1 in carry 
+                carry = 1;     // modulus with 10 to get the unit digit
                 digits[i] = temp % 10;
             } else {
-                carry = 0;
+                carry = 0;      // if not > 10, then carry is zero
                 digits[i] = temp;
             }
         }
-        if (carry != 0) {
+        if (carry != 0) {   // if carry is not zero, we create new array of size plus one the orignal
             int[] newDigits = new int[digits.length+1];
             newDigits[0] = 1;
             for (int i = 1; i < newDigits.length; i++) {
