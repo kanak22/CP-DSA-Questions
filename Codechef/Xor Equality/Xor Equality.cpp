456d47a538579157a04b3a173cea576933baefdd
Sahil Kesarwani <80140802+SahilKesarwani@users.noreply.github.com>
2021-10-17 12:33:26 -0700

    Added comments

--- Codechef/Xor Equality/Xor Equality.cpp
+++ Codechef/Xor Equality/Xor Equality.cpp
@@ -4,7 +4,7 @@
 #include <cmath>
 using namespace std;
 
-#define mod (1000000000 + 7)
+#define mod (1000000000 + 7)    // Modulo the question asked to take to store big numbers...
 
 void solve(long long int n)
 {
@@ -12,14 +12,14 @@ void solve(long long int n)
     n = n - 1;
     while (n > 0)
     {
-        if (n & 1)
+        if (n & 1)      // if (n & 1) is true that means n is odd.
         {
-            d = (d * i) % mod;
+            d = (d * i) % mod;      // Taking mod because x will be 2^(n-1) which is not possible to store.
         }
         i = (i * i) % mod;
         n = n >> 1;
     }
-    cout << d << endl;
+    cout << d << endl;      // d will be the number of possible ways of x can be taken.
 }
 
 int main()
@@ -32,7 +32,7 @@ int main()
         cin >> n;
         if (n == 2)
         {
-            cout << 2 << endl;
+            cout << 2 << endl;    // For n = 2, there will always have only two values 0, 2.
         }
         else
         {
