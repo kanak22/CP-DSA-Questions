7e7f0226e6b4ff06b2edad16108e0ba27a5f6a3a
Tanmay Tete <tanmay.tete@somaiya.edu>
2021-10-22 14:41:53 +0530

    Leetcode probolem Remove Outermost Parantheses

---
+++ LeetCode/Remove Outermost Parantheses/RemoveOuterMostParantheses.java
@@ -0,0 +1,21 @@
+//Problem Link: https://leetcode.com/problems/remove-outermost-parentheses/
+
+class Solution {
+    public String removeOuterParentheses(String s) {
+        int count=0, start=0;
+        StringBuilder res= new StringBuilder();
+        
+        for(int i=0; i<s.length(); i++){
+            if(s.charAt(i)=='(')
+                count++;
+            else
+                count--;
+				
+            if(count==0){
+                res.append(s.substring(start+1,i));
+                start=i+1;
+            }
+        }
+        return res.toString();
+    }
+}
