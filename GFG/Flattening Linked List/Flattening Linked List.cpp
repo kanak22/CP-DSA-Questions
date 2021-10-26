4e54efa4e3f880973161c96d886e67c17965f8da
Ankita Pal <ankitapal7777@gmail.com>
2021-10-26 21:28:08 +0530

    comments added

--- GFG/Flattening Linked List/Flattening Linked List.cpp
+++ GFG/Flattening Linked List/Flattening Linked List.cpp
@@ -30,29 +30,29 @@ node in a single level.
 // Solution :
 
     Node*merge(Node*l1,Node*l2){
-        Node*ans=new Node(0);
+        Node*ans=new Node(0);  //create dummy node 
         Node*one=ans;
         while(l1 && l2){
-            if(l1->data<=l2->data){
-                one->bottom=l1;
-                l1=l1->bottom;
-                one=one->bottom;
+            if(l1->data<=l2->data){ // check if both l1 and l2 have data and if data of l1->data<=l2->data
+                one->bottom=l1; //link l1 to one 
+                l1=l1->bottom; //move to next l1
+                one=one->bottom; 
             }
             else{
-                one->bottom=l2;
+                one->bottom=l2;  //link l2 to one
                 l2=l2->bottom;
                 one=one->bottom;
             }
         }
         
-        while(l1){
+        while(l1){ //if we have only l1 
             one->bottom=l1;
             one=one->bottom;
             l1=l1->bottom;
             
         }
         
-        while(l2){
+        while(l2){ // if we have only l2 
             one->bottom=l2;
             l2=l2->bottom;
             one=one->bottom;
@@ -65,13 +65,14 @@ Node *flatten(Node *root)
 {
    // Your code here
    
-   Node*ans=new Node(0);
+   Node*ans=new Node(0); //create a dummy node 
       if(root==NULL|| root->next==NULL) return root;
 
    Node*one=ans;
    Node*l2=root;
    while(l2!=NULL){
-       one->bottom=merge(one->bottom,l2);
+       one->bottom=merge(one->bottom,l2); 
+    //    we merge the list and keep linking them 
        l2=l2->next;
    }
    return ans->bottom;
