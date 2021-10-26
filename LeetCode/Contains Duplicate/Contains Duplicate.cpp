475676c53acdabc8be314003df86427860f6d67f
Ishaarora1234 <65450919+Ishaarora1234@users.noreply.github.com>
2021-10-26 23:15:06 +0530

    Update Contains Duplicate.cpp

--- LeetCode/Contains Duplicate/Contains Duplicate.cpp
+++ LeetCode/Contains Duplicate/Contains Duplicate.cpp
@@ -5,8 +5,9 @@ public:
         
         for(int i=0; i<nums.size(); i++)
         {
+            //increase the value at the array element value index of the map
             m[nums[i]]++;
-            
+            //if the frequency of the element is more than one return true
             if(m[nums[i]] >= 2)
                 return true;
         }
