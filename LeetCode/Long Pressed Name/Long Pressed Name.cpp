382a6ecea185475ba34379b13b1d1b3c23a90f8c
Ankita Pal <ankitapal7777@gmail.com>
2021-10-26 08:52:02 +0530

    comments added

--- LeetCode/Long Pressed Name/Long Pressed Name.cpp
+++ LeetCode/Long Pressed Name/Long Pressed Name.cpp
@@ -42,23 +42,27 @@ name and typed contain only lowercase English letters.
 class Solution {
 public:
     bool isLongPressedName(string a, string b) {
-        int n=a.length();
-        int l=b.length();
-        if(n>l) return false;
-        int i=0,j=0;
-        while(i<n){
-            if(a[i]==b[j]){
+        int n=a.length(); // length of a string 
+        int l=b.length();  //lenght of string b 
+        if(n>l) return false;  // invalid string 
+        int i=0,j=0; // intialise i and j 
+
+        while(i<n){ //run loop till i<n 
+            if(a[i]==b[j]){  // if a[i]==b[j] then increment i and j 
                 i++;
                 j++;
             }
-            else if(i>0 && (a[i-1]==b[j])){
+            else if(i>0 && (a[i-1]==b[j])){  
+             //if current inex of b is not equal to current index of string a then check
+            // whether is it equal to previous index of string a ,it means that it will be long pressed 
+            //then keep incrementing j 
                 j++;
             }
             else{
                 return false;
             }
         }
-        while(j<l){
+        while(j<l){  //if last index of j string is long pressesd
             if(b[j]!=a[n-1]) return false;
             j++;
 }
