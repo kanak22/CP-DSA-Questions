7b64b80ec5fa1cffec84ee59ce975bb2a6ba7823
Ankita Pal <ankitapal7777@gmail.com>
2021-10-26 21:22:41 +0530

    comments added

--- LeetCode/Rotate Image/Rotate Image.cpp
+++ LeetCode/Rotate Image/Rotate Image.cpp
@@ -25,16 +25,19 @@ Output: [[15,13,2,5],[14,3,4,1],[12,6,8,9],[16,7,10,11]]
 class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
-         int n=matrix[0].size();
+         int n=matrix[0].size(); //matrix size
+        //  we reverse the matrix around the diagonal of matrix 
      for(int i=0;i<n;i++){
          for(int j=i+1;j<n;j++){
-             if(i!=j){
+             if(i!=j){ 
                  swap(matrix[i][j],matrix[j][i]);
              }
          }
      }
   
     for(int i=0;i<n;i++){
+        // after reversing around the diagonal points 
+        //reverse each row we will get desired output 
         reverse(matrix[i].begin(),matrix[i].end());
     }         
       
