d2fde406f007478a9a0b5c3cc882f497414530ef
Your Name <you@example.com>
2021-10-28 10:52:25 +0530

    comment added

--- 450 DSA/Arrays/Kadane's algo/Kadane's algo.java
+++ 450 DSA/Arrays/Kadane's algo/Kadane's algo.java
@@ -7,6 +7,7 @@ class KadaneAlgorithm
 {
     public static void main (String[] args)
     {
+        //given Input array
         int [] a = {74,-72,94,-53,-59,-3,-66,36,-13,22,73,15,-52,75};
         System.out.println("Maximum contiguous sum is " +
                                        maxSubArraySum(a));
@@ -15,16 +16,23 @@ class KadaneAlgorithm
     static int maxSubArraySum(int a[])
     {
         int size = a.length;
+        // max calculate the maximum sum so far
+        // sum calculate the running sum of array
         int max = Integer.MIN_VALUE, sum = 0;
  
         for (int i = 0; i < size; i++)
         {
             sum = sum + a[i];
+            //if running sum is greater than maximum sum update it
             if (max <sum)
                 max = sum;
+            // if sum is -ve than start for new subarray with sum=0
             if (sum < 0)
                 sum = 0;
         }
         return max;
     }
 }
+
+// output for above input=156
+// Time complexity O(n)
