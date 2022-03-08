e62de71ffa25e9acdd94adf67b723acddfb7825b
Samarth Gupta <samarthgupta1202@gmail.com>
2022-03-08 15:42:58 +0530

    updated the expression for mid

--- LeetCode/Find peak element/Find peak element.cpp
+++ LeetCode/Find peak element/Find peak element.cpp
@@ -2,7 +2,7 @@
 int findPeakElement(vector<int>& nums) {
         int n = nums.size(), s = 0, e = n - 1;
         while(s < e) {
-            int mid = (s + e)/2;
+            int mid = s + (e-s)/2;
             if(nums[mid] < nums[mid + 1])
                 s = mid + 1;
             else
