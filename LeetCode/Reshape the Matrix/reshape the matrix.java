fa2a4b9dd2bf1ade51cebee3a07ff19095dd1ffe
Samarth Gupta <samarthgupta1202@gmail.com>
2022-03-12 14:11:54 +0530

    Java soltion for Reshape Array added

--- LeetCode/Reshape the Matrix/reshape the matrix.java
+++ LeetCode/Reshape the Matrix/reshape the matrix.java
@@ -1,7 +1,7 @@
 class Solution {
 
 public int[][] matrixReshape(int[][] mat, int r, int c) {        
-    //If mat array length is not equal to future matrix length return current array
+    // If mat array length is not equal to future matrix length return current array
     if(mat.length * mat[0].length != r*c){
         return mat;
     }        
@@ -11,7 +11,7 @@ public int[][] matrixReshape(int[][] mat, int r, int c) {
         for(int j=0;j<mat[0].length;j++){                
             newArray[x][y]= mat[i][j];                
             y++;
-            //If column reaches to max then assign new row and set column from start
+            // If column reaches to max then assign new row and set column from start
             if(y==c){
                 x++;
                 y=0;
